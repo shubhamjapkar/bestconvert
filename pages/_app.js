290261e374bebcfc654f6bcdc3fdcda433dd8cb4
shubham <shubham.japkar.squareyards.co.in>
2023-04-04 23:12:25 +0530

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
-import Layout from './Layout'
 
 export default function App({ Component, pageProps }) {
   return (
     <div className="body_part">
-      <Layout>
         <Component {...pageProps} />
-      </Layout>
     </div>
   )
 }
