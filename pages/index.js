d2bd0790eb8570bb76d9edbbc372ed7cc5e03948
shubham <shubham.japkar.squareyards.co.in>
2023-04-04 16:46:38 +0530

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,42 @@
+import Head from "next/head";
+import allpages from "./allpages.json";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <h1>shubham here</h1>
-    </>
-  )
+    <div className="indexpage">
+      <Head>
+        <title>Best Convert</title>
+        <meta
+          name="description"
+          content="Super fast convert images with best quality"
+        />
+        <meta name="keywords" content="Convert Images, Jpg to Png" />
+        <meta name="author" content="Shubham Japkar" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <h1 className="heading-Image-Convert">Images Convertor Tools</h1>
+
+      <div className="index_box">
+        {allpages.all.map((e, i) => {
+          return (
+            <div key={i}>
+              <Link href={e.url} passHref>
+                <div className="indexbox">
+                  <h2>{e.title}</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Praesentium consectetur aut dolore et vitae perspiciatis! Id
+                    recusandae, enim tenetur praesentium.
+                  </p>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+
+    </div>
+  );
 }
